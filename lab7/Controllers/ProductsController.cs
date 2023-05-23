691e785972fa905a6fa4f2d2660d9b82588b1a02
Alumno <Alumno@LAB707-14>
2023-05-22 21:57:40 -0500

    Agregar archivos de proyecto.

---
+++ lab7/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using lab7.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab7.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private NeptunoEntities db = new NeptunoEntities();
+        // GET: Products
+        public ActionResult Index() 
+          
+            {
+                return View(db.productos.ToList());
+            }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: clientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idproducto,nombreProducto,precioUnidad")] productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productos);
+        }
+    }
+}
